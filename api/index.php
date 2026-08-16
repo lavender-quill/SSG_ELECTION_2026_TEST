594c954ced0b2e3be8937daacafd458bcc4d45a0
memitsuki33 <guidomarc209@gmail.com>
2026-08-17 05:32:37 +0800

    k

--- api/index.php
+++ api/index.php
@@ -1,3 +1,8 @@
 <?php
+// Enable gzip compression to reduce payload size
+if (extension_loaded('zlib') && !ini_get('zlib.output_compression')) {
+    ob_start('ob_gzhandler');
+}
+
 // Bridge to main application
 require_once __DIR__ . '/../api-version1/index.php';
